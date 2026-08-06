a0ee10c8bb7cdd046a9fa06982ab7b9f366c79d6
hedleyh <hedleyhyde@protonmail.com>
2026-08-06 16:29:29 +0100

    add helper methods to update screen and check events

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ from settings import Settings
 from ship import Ship
 
 class AlienInvasion:
-    """Overall class to manage famew assets and behaviour."""
+    """Overall class to manage game assets and behaviour."""
     
     def __init__(self):
         """Initialise the game, and create game resources."""
@@ -17,23 +17,28 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         
-        
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen() 
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
   
 
 if __name__ == '__main__':
