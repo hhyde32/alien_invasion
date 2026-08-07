795b0aa83b0146ae92c093b409b22c1b7c4a3520
hedleyh <hedleyhyde@protonmail.com>
2026-08-07 15:22:47 +0100

    introduce bullet firing

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,11 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
-    
+
     def __init__(self):
         """Initialise the game, and create game resources."""
         pygame.init()
@@ -16,14 +17,17 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        
+        self.bullets = pygame.sprite.Group()
+
+
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen() 
+            self.bullets.update()
+            self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
@@ -44,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -51,15 +57,21 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-    
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
-  
 
 if __name__ == '__main__':
     # Make a game instance, and run the game
