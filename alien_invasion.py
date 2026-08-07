12166ac7c52417ad3a48072deacb2fd03248fdf3
hedleyh <hedleyhyde@protonmail.com>
2026-08-07 15:38:24 +0100

    add max bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,7 +30,6 @@ class AlienInvasion:
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
 
             self._update_screen()
             self.clock.tick(60)
@@ -65,8 +64,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
