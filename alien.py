e7ecbe6407b2d4488a683ef0140426d20c98ff74
hedleyh <hedleyhyde@protonmail.com>
2026-08-08 13:16:47 +0100

    add alien fleet movement

--- alien.py
+++ alien.py
@@ -22,8 +22,15 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def update(self):
-        """Move the alien to the right."""
-        self.x += self.settings.alien_speed
+        """Move the alien left or right."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
+
+    def check_edges(self):
+        """Return true if alien is at the edge of the sreen."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+
 
 
